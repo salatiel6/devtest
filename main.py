778978e381b1b754d5b8c483c969210fefd20d7d
salatiel6 <salatiel6@outlook.com>
2024-02-01 06:55:10 -0300

    feat: changes applied to follow pep08

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+import os
 import csv
-
-from src import db, Elevator, DataGenerator
+from src import Elevator, DataGenerator, ElevatorDatabase
 from flask import Flask, jsonify, request, make_response
 from io import StringIO
 
-elevator = Elevator(db)
 app = Flask(__name__)
+db = ElevatorDatabase()
 
 
-@app.route('/health', methods=['GET'])
+def configure_test():
+    global db
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    database_path = os.path.join(script_dir, "./tests/elevator_test.db")
+    db = ElevatorDatabase(database_path)
+
+
+@app.route("/health", methods=["GET"])
 def health():
-    return jsonify({'status': 'healthy'})
+    return jsonify({"status": "healthy"})
 
 
-@app.route('/generate-data', methods=['GET'])
+@app.route("/generate-data", methods=["GET"])
 def generate_data():
     try:
         with app.app_context():
@@ -21,77 +28,84 @@ def generate_data():
             data_generator = DataGenerator()
             data_generator.generate(db)
 
-        return jsonify({'message': 'Data generated successfully'}), 200
+        return jsonify({"message": "Data generated successfully"}), 200
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)}), 500
 
 
-@app.route('/call-elevator', methods=['POST'])
+@app.route("/call-elevator", methods=["POST"])
 def call_elevator():
     try:
         data = request.get_json()
-        demand_floor = data['demand_floor']
-        destination_floor = data['destination_floor']
+        demand_floor = data["demand_floor"]
+        destination_floor = data["destination_floor"]
 
         with app.app_context():
+            elevator = Elevator(db)
             elevator.call_elevator(demand_floor, destination_floor)
 
-        return jsonify({'message': 'Elevator called successfully'}), 200
+        return jsonify({"message": "Elevator called successfully"}), 200
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)}), 500
 
 
-@app.route('/get-all-rows', methods=['GET'])
+@app.route("/get-all-rows", methods=["GET"])
 def get_all_rows():
     try:
         with app.app_context():
             rows = db.get_all_rows()
 
-            result = [{
-                'id': row[0],
-                'current_floor': row[1],
-                'demand_floor': row[2],
-                'destination_floor': row[3]
-            } for row in rows]
+            result = [
+                {
+                    "id": row[0],
+                    "current_floor": row[1],
+                    "demand_floor": row[2],
+                    "destination_floor": row[3],
+                }
+                for row in rows
+            ]
 
             return jsonify(result), 200
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)}), 500
 
 
-@app.route('/update-row', methods=['PUT'])
+@app.route("/update-row", methods=["PUT"])
 def update_row():
     try:
         data = request.get_json()
 
-        if 'id' in data:
-            row_id = data['id']
+        if "id" in data:
+            row_id = data["id"]
 
-            if 'current_floor' in data:
-                db.update_current_floor(row_id, data['current_floor'])
+            if "current_floor" in data:
+                db.update_current_floor(row_id, data["current_floor"])
 
-            if 'demand_floor' in data:
-                db.update_demand_floor(row_id, data['demand_floor'])
+            if "demand_floor" in data:
+                db.update_demand_floor(row_id, data["demand_floor"])
 
-            if 'destination_floor' in data:
-                db.update_destination_floor(row_id, data['destination_floor'])
+            if "destination_floor" in data:
+                db.update_destination_floor(row_id, data["destination_floor"])
 
-            return jsonify({'message': f'Row {row_id} updated successfully'}), 200
+            return (
+                jsonify({"message": f"Row {row_id} updated successfully"}),
+                200,
+            )
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)}), 500
 
 
-@app.route('/delete-all-rows', methods=['DELETE'])
+@app.route("/delete-all-rows", methods=["DELETE"])
 def delete_all_rows():
     try:
         with app.app_context():
             db.delete_all_rows()
-        return jsonify({'message': 'All rows deleted successfully'}), 200
+        return jsonify({"message": "All rows deleted successfully"}), 200
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)}), 500
 
 
-@app.route('/export-csv', methods=['GET'])
+@app.route("/export-csv", methods=["GET"])
 def export_csv():
     try:
         with app.app_context():
@@ -100,7 +114,9 @@ def export_csv():
             csv_writer = csv.writer(csv_data)
 
             # Escreve o cabeçalho
-            csv_writer.writerow(['id', 'current_floor', 'demand_floor', 'destination_floor'])
+            csv_writer.writerow(
+                ["id", "current_floor", "demand_floor", "destination_floor"]
+            )
 
             # Escreve os dados
             csv_writer.writerows(rows)
@@ -108,13 +124,15 @@ def export_csv():
             csv_data.seek(0)
 
         response = make_response(csv_data.getvalue())
-        response.headers['Content-Disposition'] = 'attachment; filename=elevator_data.csv'
-        response.headers['Content-Type'] = 'text/csv'
+        response.headers[
+            "Content-Disposition"
+        ] = "attachment; filename=elevator_data.csv"
+        response.headers["Content-Type"] = "text/csv"
 
         return response
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)}), 500
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
--- src/data_generator.py
+++ src/data_generator.py
@@ -9,19 +9,19 @@ class DataGenerator:
         script_dir = os.path.dirname(os.path.abspath(__file__))
 
         # Constrói o caminho completo para o arquivo JSON
-        json_path = os.path.join(script_dir, 'elevator_travels.json')
+        json_path = os.path.join(script_dir, "elevator_travels.json")
 
         # Verifica se o arquivo existe antes de tentar abri-lo
         if os.path.exists(json_path):
             # Carrega os dados do arquivo JSON
-            with open(json_path, 'r') as file:
+            with open(json_path, "r") as file:
                 data = json.load(file)
 
             # Itera sobre os dados e insere no banco de dados
             for travel in data:
-                current_floor = travel['current_floor']
-                demand_floor = travel['demand_floor']
-                destination_floor = travel['destination_floor']
+                current_floor = travel["current_floor"]
+                demand_floor = travel["demand_floor"]
+                destination_floor = travel["destination_floor"]
 
                 db.insert_call(current_floor, demand_floor, destination_floor)
         else:
