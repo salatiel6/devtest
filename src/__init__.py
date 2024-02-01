230fa45fbb30478490a455de31d05bc42fd7ae4f
salatiel6 <salatiel6@outlook.com>
2024-02-01 06:54:36 -0300

    feat: ignored F401 on init

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from .db import db
-from .db import ElevatorDatabase
-from .elevator import Elevator
-from .data_generator import DataGenerator
+from .db import db  # noqa: F401
+from .db import ElevatorDatabase  # noqa: F401
+from .elevator import Elevator  # noqa: F401
+from .data_generator import DataGenerator  # noqa: F401
